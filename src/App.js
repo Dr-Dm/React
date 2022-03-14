3076af44727163db66428d0d6be3e68ee3c9d28f
Dr-Dm <0557586@gmail.com>
2022-03-14 23:31:33 +0300

    3 без стилизации

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import React, { useState } from 'react';
 export function App() {
 
   const [messageList, setMessageList] = useState([]);
-  //const [value, setValue] = useState('');
+  const [text, setText] = useState('');
+  const [autor, setAutor] = useState('');
   
   const sendMessage = (message) => {
-    setMessageList([...messageList, message])
+    setMessageList([...messageList, message]);
+    setText('');
+    setAutor('');
   }
-
 
   return (
     <div className="App">
       <header className="App-header">
-      <div className=''>{messageList.map((object) => (
-      <div>
-        <h2 className=''>{object.author}</h2>
-        <p className=''>{object.text}</p>
-      </div>
-    ))}</div>
-        <button onClick={() => sendMessage({author: "sdfjhsd", text: "dfsf"})}>sendMessage</button>
+      <div className=''>
+      {messageList.map((object, index) => (
+        <div key={index}>
+          <h2 className=''>{object.author}</h2>
+          <p className=''>{object.text}</p>
+        </div>
+      ))}
+    </div>
+        <input placeholder="Сообщение" value={text} onChange={event => setText(event.target.value)} />
+        <input placeholder="Autor" value={autor} onChange={event => setAutor(event.target.value)} />
+        <button onClick={() => sendMessage({author: autor, text: text})}>sendMessage</button>
       </header>
     </div>
   );
 }
-
