cf6d2701bac30bad5903c986c3867cc5f680fb1a
Dr-Dm <0557586@gmail.com>
2022-04-07 20:57:57 +0300

    hw5

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';
-import { MessagesList, Layout, ChatList, Header } from './components';
-import { ProfilePage, ChatPage } from './pages';
+import "./index.css";
+import { MessagesList, Layout, ChatList, Header } from "./components";
+import { ProfilePage, ChatPage } from "./pages";
+import { store } from "./store";
 
-// import "./global.css";
+//import "./global.css";
 
-const App = () => {
-  return (
-    <>
-      <Layout messages={<MessagesList />} 
-      chats={<ChatList />} 
-      header={<Header />} />
-    </>
-  );
-};
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>}/>
-        <Route path="profile" element={<ProfilePage />}/>
-        <Route path="/chat/*" element={<ChatPage />}/>
-        <Route path="*" element={<h1>404 not found</h1>}/>
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="/chat/*" element={<ChatPage />} />
+          <Route path="*" element={<h1>404 not found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
