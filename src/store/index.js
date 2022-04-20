caae5224848bce4c66e034d2217bc741467f45d4
Dr-Dm <0557586@gmail.com>
2022-04-20 09:43:55 +0300

    8

--- src/store/index.js
+++ src/store/index.js
@@ -2,32 +2,47 @@ import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
+import { getPublicGistsApi } from "../api/gists";
 import { profileReducer } from "./profile";
 import { conversationsReducer } from "./conversations";
 import { messagesReducer } from "./messages";
-import { logger, botMessage, timeScheduler, crashReporter } from "./middlewares";
+import { gistsReducer } from "./gists";
 
+import {
+  logger,
+  botMessage,
+  timeScheduler,
+  crashReporter,
+} from "./middlewares";
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  blacklist: ['messages'],
-  whitelist: ['profile'],
+  blacklist: ["messages"],
+  whitelist: ["profile"],
 };
 
 const reducer = combineReducers({
   profile: profileReducer,
   conversations: conversationsReducer,
   messages: messagesReducer,
-})
+  gists: gistsReducer,
+});
 
 export const store = createStore(
   persistReducer(persistConfig, reducer),
   compose(
-    applyMiddleware(crashReporter, thunk, logger, botMessage, timeScheduler),
-  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (args) => args
+    applyMiddleware(
+      crashReporter,
+      thunk.withExtraArgument({ getPublicGistsApi }),
+      logger,
+      botMessage,
+      timeScheduler
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (args) => args
   )
-  
 );
 
 export const persistor = persistStore(store);
